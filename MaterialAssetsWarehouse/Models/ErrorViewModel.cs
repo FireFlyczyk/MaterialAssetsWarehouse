2294a22ad5cc84b721094fc134d812839f21a6e7
FireFlyczyk <107504085+FireFlyczyk@users.noreply.github.com>
2024-03-14 18:33:01 +0100

    Add project files.

---
+++ MaterialAssetsWarehouse/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MaterialAssetsWarehouse.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ MaterialAssetsWarehouse/Models/Item.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MaterialAssetsWarehouse.Models
+{
+    public class Item
+    {
+        public int ItemID { get; set; }
+
+        public ItemGroup Group { get; set; }
+
+        public UnitOfMeasurement Measurement { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Status { get; set; }
+
+        public string StorageLocation { get; set; }
+
+        public string ContactPerson { get; set; }
+
+        public string Photo { get; set; }
+
+        public enum ItemGroup
+        {
+            Stationery,
+            Food,
+            Production,
+            Vehicle,
+
+        }
+
+        public enum UnitOfMeasurement
+        {
+            Gram,
+            Kilogram,
+            Litre,
+            Meter,
+            Not_Applicable
+        }
+    }
+}
