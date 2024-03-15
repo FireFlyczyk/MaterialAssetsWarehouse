51061c1fea20c3d5373ab674183c9a7fb2505072
FireFlyczyk <107504085+FireFlyczyk@users.noreply.github.com>
2024-03-15 22:23:02 +0100

    Sorting added

--- MaterialAssetsWarehouse/Controllers/ItemController.cs
+++ MaterialAssetsWarehouse/Controllers/ItemController.cs
@@ -1,4 +1,5 @@
 ﻿using MaterialAssetsWarehouse.Data;
+using MaterialAssetsWarehouse.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MaterialAssetsWarehouse.Controllers
@@ -13,10 +14,39 @@ namespace MaterialAssetsWarehouse.Controllers
         }
 
 
-        public IActionResult Items()
+        public IActionResult Items(string sortOrder)
         {
+            ViewBag.IdSortParm = String.IsNullOrEmpty(sortOrder) ? "ItemId_desc" : "";
+            ViewBag.GroupSortParm = String.IsNullOrEmpty(sortOrder) ? "Group_desc" : "";
+            ViewBag.MeasurementSortParm = sortOrder == "Measurement" ? "Measurement_desc" : "Measurement";
+            ViewBag.QuantitySortParm = sortOrder == "Quantity" ? "Quantity_desc" : "Quantity";
+            ViewBag.PriceWithoutVATSortParm = sortOrder == "PriceWithoutVAT" ? "PriceWithoutVAT_desc" : "PriceWithoutVAT";
+            ViewBag.StatusSortParm = sortOrder == "Status" ? "Status_desc" : "Status";
+            ViewBag.StorageLocationSortParm = sortOrder == "StorageLocation" ? "StorageLocation_desc" : "StorageLocation";
+            ViewBag.ContactPersonSortParm = sortOrder == "ContactPerson" ? "ContactPerson_desc" : "ContactPerson";
+
             var items = _itemRepository.GetAllItems();
-            return View(items);
+            items = sortOrder switch
+            {
+                "ItemId_desc" => items.OrderByDescending(i => i.ItemID),
+                "Group_desc" => items.OrderByDescending(i => i.Group),
+                "Measurement" => items.OrderBy(i => i.Measurement),
+                "Measurement_desc" => items.OrderByDescending(i => i.Measurement),
+                "Quantity" => items.OrderBy(i => i.Quantity),
+                "Quantity_desc" => items.OrderByDescending(i => i.Quantity),
+                "PriceWithoutVAT" => items.OrderBy(i => i.PriceWithoutVAT),
+                "PriceWithoutVAT_desc" => items.OrderByDescending(i => i.PriceWithoutVAT),
+                "Status" => items.OrderBy(i => i.Status),
+                "Status_desc" => items.OrderByDescending(i => i.Status),
+                "StorageLocation" => items.OrderBy(i => i.StorageLocation),
+                "StorageLocation_desc" => items.OrderByDescending(i => i.StorageLocation),
+                "ContactPerson" => items.OrderBy(i => i.ContactPerson),
+                "ContactPerson_desc" => items.OrderByDescending(i => i.ContactPerson),
+                _ => items.OrderBy(i => i.ItemID),
+            };
+            return View(items.ToList());
         }
+
     }
-}
+        
+}  
